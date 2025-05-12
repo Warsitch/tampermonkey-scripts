9e417e4c992806c71d0b90dae574aa77ef19dd7b
Warsitch <ramakersal@gmail.com>
2025-05-12 14:07:58 +0200

    Update Sand-AdmissionHospit.user.js

--- Sand-AdmissionHospit.user.js
+++ Sand-AdmissionHospit.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Sand-AdmissionHospit
 // @namespace    https://www.chu-brugmann.be/fr/
-// @version      0.3
+// @version      1.0
 // @description  Inscription facilitée dans WISH
 // @author       Ramakers Alexandre
 // @match        http://br400prd.chu-brugmann.be:30700/*
 // @grant        GM_addStyle
-// @updateURL    https://raw.githubusercontent.com/Warsitch/tampermonkey-scripts/main/sand-AdmissionHospit.user.js
-// @downloadURL  https://raw.githubusercontent.com/Warsitch/tampermonkey-scripts/main/sand-AdmissionHospit.user.js
+// @updateURL    https://github.com/Warsitch/tampermonkey-scripts/raw/refs/heads/main/Sand-AdmissionHospit.user.js
+// @downloadURL  https://github.com/Warsitch/tampermonkey-scripts/raw/refs/heads/main/Sand-AdmissionHospit.user.js
 // ==/UserScript==
 
 /*
